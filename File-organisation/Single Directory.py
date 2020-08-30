4b7610962c2945a83137238311269615784281f5
anto-pv <antopv833@gmail.com>
2020-08-30 22:40:05 +0530

    Added file organisation exp

---
+++ File-organisation/Single Directory.py
@@ -0,0 +1,54 @@
+from termcolor import colored
+files = []
+error = 0
+def printDirectory():
+    if(len(files) == 0):
+        print("--------------------------\n | Empty Directory | \n--------------------------")
+    else:
+        # print("---------------------------------------\n root directory\n---------------------------------------\n")
+        for file in files:
+            print(colored(f"{file}", "cyan"), end="   ")
+        print('\n')
+        # print("\n---------------------------------------\n")
+
+def addFile(fname):
+    # fname = input("Enter File name : ")
+    if fname not in files:
+        files.append(fname)
+    else:
+        print(colored("Error: File already exist!!\n",'red'))
+    
+def deleteFile(fname):
+    # fname = input("Enter File Name : ")
+    if fname not in files:
+        print(colored("Error: File doesn't exist!!\n",'red'))
+        return
+    files.remove(fname)
+    
+
+print("Type help to list commands")
+while(1):
+    print(colored("anto@SSlab >> ","green"), end=" ")
+    choice =input()
+    command = choice.split(' ')[0]
+    try:
+        filename = choice.split(' ')[1]
+    except:
+        pass
+    if(command== "ls"):
+        printDirectory()
+    elif(command == "touch"):
+        addFile(filename)
+    elif(command == "rm"):
+        deleteFile(filename)
+    elif(command == "exit"):
+        exit()
+    elif(command == "help"):
+        print("----------------------------------------\nman page\n----------------------------------------")
+        print("%-20s %-20s" % ("ls", "List Files"))
+        print("%-20s %-20s" % ("touch <filename>", "Create a file"))
+        print("%-20s %-20s" % ("rm <filename>", "Delete a file"))
+        print("%-20s %-20s" % ("exit", "Exit the shell"))
+        print("----------------------------------------\n")
+    else:
+        print("Wrong choice!!\n")
---
+++ File-organisation/Two level Directory.py
@@ -0,0 +1,104 @@
+from termcolor import colored
+class Directory:
+
+    def __init__(self, name, level):
+        self.name = name
+        self.files = []
+        self.directories = []
+        self.level = level
+
+    def getName(self):
+        return self.name
+
+    def touch(self, filename):
+        self.files.append(filename)
+
+    def rm(self, filename):
+        if filename in self.files:
+            self.files.remove(filename) 
+            return
+        print("Error : File doesn't exists")
+    
+    def mkdir(self, directory_name):
+        next_level = self.level + 1
+        if(next_level < 3):
+            self.directories.append(Directory(directory_name,next_level))
+        else:
+            print("Error: Only two levels are possible")
+
+    def rmdir(self, directory_name):
+        found = 0
+        for directory in self.directories:
+            if (directory.name == directory_name):
+                found = 1
+                self.directories.remove(directory)
+                break
+        if(not found):
+            print("Error: Directory doesn't exist")
+
+    def ls(self):
+        for file in self.files:
+            print(colored(f"{file}", "cyan"), end="   ")
+        for directory in self.directories:
+            print(colored(f"{directory.name}", "green"), end="   ")
+        print()
+    
+    def open(self, directory_name):
+        found = 0
+        for directory in self.directories:
+            if (directory.name == directory_name):
+                found = 1
+                directory.ls()
+                break
+        if(not found):
+            print("Error: Directory doesn't exist")
+
+    def touchInto(self, directory_name, filename):
+        found = 0
+        for directory in self.directories:
+            if (directory.name == directory_name):
+                found = 1
+                directory.touch(filename)
+                break
+        if(not found):
+            print("Error: Directory doesn't exist")
+
+def main():
+    root = Directory("bin", 1)
+    # current_dir = root
+    # root.touch('main.py') 
+    # root.touch('fast.py')
+    # root.mkdir('projects')
+    # root.ls()
+    # root.touchInto('projects', 'project1')
+    # root.touchInto('projects', 'project2')
+    # root.open('projects')
+    # print()
+    while(1):
+        print(colored("anto@SSlab >> ","green"), end=" ")
+        choice =input()
+        command = choice.split(' ')[0]
+        try:
+            name = choice.split(' ')[1]
+            option = choice.split(' ')[2]
+        except:
+            pass
+        if(command == 'mkdir'):
+            root.mkdir(name)
+        elif(command == "touch"):
+            root.touch(name)
+        elif(command == "ls"):
+            root.ls()
+        elif(command == "open"):
+            root.open(name)
+        elif(command == "touchInto"):
+            root.touchInto(name, option)
+        elif(command == "rm"):
+            root.rm(name)
+        elif(command == "rmdir"):
+            root.rmdir(name)
+        elif(command == "exit"):
+            exit()
+        else:
+            print("Error: Wrond Command")
+main();
