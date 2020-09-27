16cd0e87f5640c173fa09895ef10752182b3170a
anto-pv <antopv833@gmail.com>
2020-09-27 09:22:40 +0530

    added dining philosper and producer consumer

---
+++ Dining Philosper Problem/dining philosper.py
@@ -0,0 +1,44 @@
+import threading
+import time
+class Philosopher(threading.Thread):
+	running = True
+	def __init__(self,index,left,right):
+		threading.Thread.__init__(self)
+		self.index = index
+		self.left = left
+		self.right = right
+
+	def run(self):
+		while (self.running):
+			time.sleep(10)
+			print(f"Philosopher {self.index} is hungry")
+			self.dine()
+
+	def dine(self):
+		lft, rgt = self.left, self.right
+		while self.running:
+			lft.acquire()
+			locked = rgt.acquire(False)
+			if locked: break
+			lft.release()
+		else:
+			return
+		self.dining()
+		lft.release()
+		rgt.release()
+   
+	def dining(self):
+		print(f"Philosopher {self.index} starts eating")
+		time.sleep(10)#Eating
+		print(f"Philosopher {self.index} finishes eating and starts thinking")
+
+N = int(input("Enter the number of Philosophers: "))
+forks = [threading.Semaphore() for i in range(N)]
+philosophers = [Philosopher(i,forks[i%N],forks[(i+1)%N]) for i in range(N)]
+Philosopher.running = True
+for p in philosophers:
+	p.start()
+time.sleep(30)
+Philosopher.running = False
+time.sleep(10)
+print("Finish!")
